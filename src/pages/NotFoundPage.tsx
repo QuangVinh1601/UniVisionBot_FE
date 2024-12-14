8cd2a9a633b338cc779337cac58f9d823cf83118
Trang Minh Trí <86817635+king1107@users.noreply.github.com>
2024-12-14 15:11:03 +0700

    update navigate version 2

--- src/pages/NotFoundPage.tsx
+++ src/pages/NotFoundPage.tsx
@@ -1,14 +1,30 @@
 import React from 'react';
 import './NotFoundPage.css';
+import { useNavigate } from 'react-router-dom';
 
 const NotFoundPage: React.FC = () => {
+  const navigate = useNavigate();
+
+  const handleBackClick = () => {
+    const role = localStorage.getItem('role');
+    if (role === 'ADMIN') {
+      navigate('/admin'); // Chuyển hướng về trang admin
+    } else if (role === 'CONSULTANT') {
+      navigate('/consultant-chat'); // Chuyển hướng về trang consultant
+    } else {
+      navigate('/'); // Chuyển hướng về trang Home
+    }
+  };
+
   return (
     <div className="NotFound-container">
       <div className="NotFound-error">
         <h1>Uh Ohh!</h1>
         <p>We couldn't find the page that you're looking for :(</p>
         <div className="NotFound-cta">
-          <button className="NotFound-cta-back" onClick={() => window.history.back()}>Go Back</button>
+          <button className="NotFound-cta-back" onClick={handleBackClick}>
+            Quay lại
+          </button>
         </div>
       </div>
       <img
